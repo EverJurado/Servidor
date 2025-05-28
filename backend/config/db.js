999b2560b4cdb8892b51df969d750661d7b3daaa
EverJurado <ejuradoj@fcpn.edu.bo>
2025-05-28 14:17:54 -0400

    nueva forma

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,14 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // necesario para conexiones externas como Railway
+  },
 });
 
 pool.connect()
-    .then(() => console.log("🟢 Conexión exitosa con PostgreSQL"))
-    .catch(err => console.error("🔴 Error al conectar con PostgreSQL:", err.message));
+  .then(() => console.log("🟢 Conexión exitosa con PostgreSQL"))
+  .catch(err => console.error("🔴 Error al conectar con PostgreSQL:", err.message));
 
 module.exports = pool;
