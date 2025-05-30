aeb22f489a3b9f08bf822ba8cbddb80c401d7ad0
EverJurado <ejuradoj@fcpn.edu.bo>
2025-05-30 11:18:50 -0400

    DASH

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -2,7 +2,7 @@ const db = require('../config/db');
 
 const getUsuariosPorRol = async (req, res) => {
   try {
-    const result = await db.query("SELECT nombre, id_rol FROM Usuarios");
+    const result = await db.query("SELECT nombre, id_rol FROM usuarios");
     res.json(result.rows);
   } catch (error) {
     console.error("Error al obtener usuarios:", error);
